566be798da02c0cc04a2058ae261601ef5f8a1bf
subair44 <subairmohamed44@gmail.com>
2022-03-13 10:24:28 -0700

    first-changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 
 function App() {
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className="flex min-h-screen">
+      {/* aside left */}
+
+      <aside id="left" className="bg-blue-400 w-20 lg:w-28 flex-none">
+        left
+      </aside>
+      {/* main */}
+
+      <section id="main" className="bg-green-200 grow">
+        <header>header</header>
+        <main>main</main>
+      </section>
+
+      {/* aside right */}
+
+      <aside id="right" className="bg-red-200 sm:hidden md:block md:w-80 lg:w-96">
+        right
+      </aside>
+
     </div>
   );
 }
