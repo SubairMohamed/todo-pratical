c9ee14aae13b1acbd2f0bf4d462b277f9470f2d9
subair44 <subairmohamed44@gmail.com>
2022-03-16 10:51:57 -0700

    fourth-changes

--- src/components/Asides/Right.jsx
+++ src/components/Asides/Right.jsx
@@ -2,7 +2,7 @@ const Right = () => {
     return (
 
         // aside right 
-        <aside id="right" className="sm:hidden md:block md:w-80 lg:w-96 bg-white">
+        <aside id="right" className="sm:hidden md:block md:w-80 lg:w-96 bg-white rounded-tr-xl">
             right
         </aside>
     )
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,18 @@
+import { PlusCircleIcon } from '@heroicons/react/solid';
+
 const Main = () => {
     return (
         // main
         <section id="main" className="grow font-nunito">
             <header className="mx-5 my-5 flex items-center justify-between">
                 <div>
-                    <h1 className="text-lg font-bold tracking-wider">My Todo App</h1>
+                    <h1 className="text-lg font-bold tracking-wider bg-yellow-600 text-white rounded-lg p-1 mb-1">My Todo App</h1>
                     <p className="text-gray-50 text-sm tracking-wider">Sava my todo app</p>
                 </div>
-                <div className="bg-yellow-500 text-white p-1 rounded-full">
-                    <button>Add</button>
+                <div className="bg-yellow-500 text-white rounded-full flex justify-center items-center">
+                    <button>
+                        <PlusCircleIcon className='w-5 h-5 text-yellow-100' />
+                    </button>
                 </div>
             </header>
             <main>main</main>
