0962708367be1456844460f56b754466fa4c9a75
subair44 <subairmohamed44@gmail.com>
2022-03-30 21:13:54 +0300

    ninth-changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,10 +9,6 @@ const Main = () => {
         // main
         <section id="main" className="grow font-nunito">
             <header className="mx-5 my-5 flex items-center justify-between">
-                <div>
-                    <h1 className="text-lg font-bold tracking-wider bg-yellow-600 text-white rounded-lg p-1 mb-1">My Todo App</h1>
-                    <p className="text-gray-50 text-sm tracking-wider">Sava my todo app</p>
-                </div>
                 <div className="bg-yellow-500 text-white rounded-full flex justify-center items-center">
                     <button>
                         <PlusCircleIcon className='w-5 h-5 text-yellow-100' />
